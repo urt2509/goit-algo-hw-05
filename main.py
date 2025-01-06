877764a8275cab7f91cef5efabdbdaec948b657a
Alla <urt2509@gmail.com>
2025-01-06 11:16:23 +0100

    task_1 updated

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-def caching_fibonacci():
-    cache = {}
+from fibonacci import fib
 
-    def fibonacci(n):
-
-        if n <= 0:
-            return 0
-        elif n == 1:
-            return 1
-        elif cache.get(n) == n:
-            return cache[n]
-        else:
-
-            cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-            return cache[n]
-
-    return fibonacci
-
-
-fib = caching_fibonacci()
-
-print(fib(10))
-print(fib(15))
+if __name__ == "__main__":
+    print(fib(10))  # Run Task1
+    print(fib(15))  # Run Task1
